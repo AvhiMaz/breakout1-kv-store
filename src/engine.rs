a33e98361423b8cfd6ff2dce8e0438e64ed878bc
AvhiMaz <avhimazumder5@outlook.com>
2026-02-21 12:47:30 +0530

    feat: add set fn
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/engine.rs
+++ src/engine.rs
@@ -67,3 +67,56 @@ impl Engine {
 
         Ok(())
     }
+
+    pub fn set(&mut self, key: Vec<u8>, value: Vec<u8>) -> io::Result<()> {
+        let tstamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .map(|d| d.as_millis() as i64)
+            .unwrap_or(0);
+
+        let entry = DataFileEntry {
+            tstamp,
+            key: key.clone(),
+            value: Some(value),
+        };
+
+        let data = wincode::serialize(&entry)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+        let entry_len = data.len() as u64;
+
+        self.file.seek(SeekFrom::End(0))?;
+        self.file.write_all(&entry_len.to_le_bytes())?;
+
+        let data_pos = self.file.stream_position()?;
+        self.file.write_all(&data)?;
+        self.file.flush()?;
+
+        self.index.insert(
+            key,
+            LogIndex {
+                pos: data_pos,
+                len: entry_len,
+            },
+        );
+
+        Ok(())
+    }
+
+    pub fn get(&mut self, key: &[u8]) -> io::Result<Option<Vec<u8>>> {
+        let log_index = match self.index.get(key) {
+            Some(idx) => idx.clone(),
+            None => return Ok(None),
+        };
+
+        self.file.seek(SeekFrom::Start(log_index.pos))?;
+
+        let mut data = vec![0u8; log_index.len as usize];
+        self.file.read_exact(&mut data)?;
+
+        let entry: DataFileEntry = wincode::deserialize(&data)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
+
+        Ok(entry.value)
+    }
+}
